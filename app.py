08f51632ad6489a07642436b17ab4cb3fe3d0bc6
Craig Wessel <craigwessel9@gmail.com>
2023-12-08 12:02:22 -0800

    added abort to flask input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory
+from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory, abort
 from flask_basicauth import BasicAuth
 from werkzeug.utils import secure_filename
 from paramiko import RSAKey, SSHException
