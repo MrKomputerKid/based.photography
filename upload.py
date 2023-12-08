9e234cabd4fdedd8e1fe1332b0215cb159fdc7cf
Craig Wessel <craigwessel9@gmail.com>
2023-12-08 15:23:44 -0800

    I forgot to modify upload.py whoops!

--- upload.py
+++ upload.py
@@ -1,34 +1,48 @@
-import paramiko
-import sys
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
+from werkzeug.utils import secure_filename
+from getpass import getuser
+import os
 
-def upload_via_ssh(file_path):
-    host = 'your_ssh_host'
-    port = 22
-    username = 'your_ssh_username'
-    private_key_path = '/path/to/your/private/key'  # Replace with the path to your private key
-    remote_path = '/path/to/server/uploads/'
+app = Flask(__name__)
+basic_auth = BasicAuth(app)
 
-    # Load the private key
-    private_key = paramiko.RSAKey(filename=private_key_path)
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-    # Establish an SSH connection using the private key
-    transport = paramiko.Transport((host, port))
-    transport.connect(username=username, pkey=private_key)
+# Set basic authentication creds
+app.config['BASIC_AUTH_USERNAME'] = getuser()
+app.config['BASIC_AUTH_PASSWORD'] = 'UR_PASS'
 
-    # Upload the file
-    sftp = paramiko.SFTPClient.from_transport(transport)
-    sftp.put(file_path, remote_path + file_path)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-    # Close the connection
-    sftp.close()
-    transport.close()
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python upload.py <file_path>")
-        sys.exit(1)
+@app.route('/upload', methods=['POST'])
+@basic_auth.required
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
 
-    file_path = sys.argv[1]
-    upload_via_ssh(file_path)
-    print(f"File {file_path} uploaded successfully.")
+    file = request.files['file']
 
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        upload_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        if os.path.exists(upload_path):
+            return jsonify({'error': 'File already exists'}), 400
+
+        file.save(upload_path)
+        return jsonify({'success': True, 'message': 'File uploaded successfully'}), 200
+
+    return jsonify({'error': 'Invalid file type'}), 400
+
+if __name__ == '__main__':
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+    app.run(debug=True)
