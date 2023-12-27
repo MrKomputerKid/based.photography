ed2e920846e35bd68a72d2877afe10e04fed5902
MrKomputerKid <113312570+MrKomputerKid@users.noreply.github.com>
2023-12-26 23:26:46 -0800

    Removed Google Auth from Upload.py
    Removed Google Authentication from upload.py

--- upload.py
+++ upload.py
@@ -8,55 +8,6 @@ import os
 app = Flask(__name__)
 app.secret_key = 'YOUR_SECRET_KEY'
 
-oauth = OAuth(app)
-
-google = oauth.remote_app(
-    'google',
-    consumer_key='your_google_client_id',
-    consumer_secret='your_google_client_secret',
-    request_token_params={
-        'scope': 'email',
-    },
-    base_url='https://www.googleapis.com/oauth2/v1/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-)
-
-@app.route('/')
-def index():
-    if 'google_token' in session:
-        me = google.get('userinfo')
-        return f'Logged in as: {me.data["email"]}'
-    return 'Not logged in'
-
-@app.route('/login')
-def login():
-    return google.authorize(callback=app.url_for('authorized', _external=True))
-
-@app.route('/logout')
-def logout():
-    session.pop('google_token', None)
-    return flask.redirect(app.url_for('index'))
-
-@app.route('/login/authorized')
-def authorized():
-    response = google.authorized_response()
-    if response is None or response.get('access_token') is None:
-        return 'Access denied: reason={} error={}'.format(
-            flask.request.args['error_reason'],
-            flask.request.args['error_description']
-        )
-
-    session['google_token'] = (response['access_token'], '')
-    me = google.get('userinfo')
-    return 'Logged in as: ' + str(me.data)
-
-@google.tokengetter
-def get_google_oauth_token():
-    return session.get('google_token')
-
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
